65d86ead93af1326cfc1ee2d0714bd3574a843d5
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-10-29 11:47:36 +0100

    Initial commit for LMS system

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,14 @@
+<!-- resources/views/layouts/app.blade.php -->
+ 
+<html>
+    <head>
+        <title>Laravel CRUD</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    </head>
+    <body> 
+        <div class="container">
+            @yield('content')
+        </div>
+
+    </body>
+</html>
