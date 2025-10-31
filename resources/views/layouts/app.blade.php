a8bdd8ebdf52dea8ba1380bed2743d16ba3ecb5a
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-10-31 21:16:29 +0100

    installed bootstrap icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,48 +19,6 @@
         </a>
     </div>
 
-{{--    <nav class="navbar bg-body-tertiary fixed-top">--}}
-{{--        <div class="container-fluid">--}}
-{{--            <a class="navbar-brand" href="/">Offcanvas navbar</a>--}}
-{{--            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">--}}
-{{--                <span class="navbar-toggler-icon"></span>--}}
-{{--            </button>--}}
-{{--            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">--}}
-{{--                <div class="offcanvas-header">--}}
-{{--                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>--}}
-{{--                </div>--}}
-{{--                <div class="offcanvas-body">--}}
-{{--                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">--}}
-{{--                        <li class="nav-item">--}}
-{{--                            <x-elements.link class="nav-link active" href="/" title="Dashboard" aria-current="page"></x-elements.link>--}}
-{{--                        </li>--}}
-
-{{--                        <x-blocks.dropdown href="/clients" dropdownTitle="Clients">--}}
-{{--                            <li><x-elements.link class="dropdown-item" title="Users" href="/users"></x-elements.link></li>--}}
-{{--                            <li><x-elements.link class="dropdown-item" title="Sites" href="/sites"></x-elements.link></li>--}}
-{{--                            <li><x-elements.link class="dropdown-item" title="User groups" href="/training_users"></x-elements.link></li>--}}
-{{--                        </x-blocks.dropdown>--}}
-
-{{--                        <x-blocks.dropdown href="/trainings" dropdownTitle="Trainings">--}}
-{{--                            <li><x-elements.link class="dropdown-item" title="Upcoming trainings" href="/trainings/upcoming"></x-elements.link></li>--}}
-{{--                            <li><x-elements.link class="dropdown-item" title="Completed trainings" href="/trainings/completed"></x-elements.link></li>--}}
-{{--                        </x-blocks.dropdown>--}}
-
-{{--                        <x-blocks.dropdown href="/courses" dropdownTitle="Courses">--}}
-{{--                            <li><x-elements.link class="dropdown-item" title="Certificate" href="/certificate"></x-elements.link></li>--}}
-{{--                        </x-blocks.dropdown>--}}
-
-{{--                        <li class="nav-item">--}}
-{{--                            <x-elements.link class="nav-link" href="/gdpr" title="GDPR & consents"></x-elements.link>--}}
-{{--                        </li>--}}
-{{--                    </ul>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </nav>--}}
-
-
-
 {{--    <ul class="nav justify-content-end">--}}
 {{--        <li class="nav-item">--}}
 {{--            <a class="nav-link" href="#">Link</a>--}}
